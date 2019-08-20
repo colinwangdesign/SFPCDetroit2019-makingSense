abc3122bf1c50d93102f7207c352a1bbdd6f9b9d
Colin Wang <30778227+colinwangdesign@users.noreply.github.com>
2019-08-19 23:40:28 -0400

    Create MakingSense_Comments_1_LED.py

---
+++ Class Code Commented/MakingSense_Comments_1_LED.py
@@ -0,0 +1,10 @@
+from gpiozero import LED as  # Imports the LED functions from the GPIO library
+from time import sleep       # Imports the sleep functions from the time library
+
+led = LED(21)    # Initializes pin 21 as an LED and defines variable name "led"
+
+while True:      # Run the indented code forever
+    led.on()     # Turns "led" on
+    sleep(.5)    # Waits 0.5 seconds before next line of code
+    led.off()    # Turns "led" off
+    sleep(.5)    # Waits 0.5 seconds before next line of code, in this case back to led.on()
