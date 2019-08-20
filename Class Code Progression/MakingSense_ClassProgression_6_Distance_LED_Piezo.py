048d78a62c2a27e55c865aca0207e12510524f54
Colin Wang <30778227+colinwangdesign@users.noreply.github.com>
2019-08-20 00:06:15 -0400

    Create MakingSense_ClassProgression_6_Distance_LED_Piezo.py

---
+++ Class Code Progression/MakingSense_ClassProgression_6_Distance_LED_Piezo.py
@@ -0,0 +1,33 @@
+from gpiozero import LED, Button, DistanceSensor
+from time import sleep
+
+led = LED(21)
+piezo = LED(12)
+#button = Button(16)
+#
+#button.when_pressed = piezo.off
+#button.when_released = piezo.on
+
+#while True:
+#    if button.is_pressed:
+#        led.off()
+#    else:
+#        led.on()
+
+distanceMeters = DistanceSensor(trigger = 8, echo = 11, threshold_distance = 0.15)
+
+while True:
+    distanceInches = distanceMeters.distance * 39.37
+    distanceInches = round(distanceInches, 1)
+    
+    print('Distance: ', distanceInches, 'in')
+    
+    distanceMeters.when_in_range = led.on
+    distanceMeters.when_out_of_range = led.off
+
+    if distanceInches <= 3:
+        piezo.on()
+    else:
+        piezo.off()
+        
+    sleep(.1)
